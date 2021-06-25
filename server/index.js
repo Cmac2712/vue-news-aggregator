0652bff9d39cfa0762ac44933d2b4939f91c26c0
Craig MacIntyre <craig.macintyre@iconagency.com.au>
2021-06-25 16:48:20 +1000

    Updates

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const axios = require('axios');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 let items = [];
 var sources = [];
 var normalizedPath = require("path").join(__dirname, "scrapers");
